2309de03cc7373ac22da16bd77f3136276ab2b73
mingyeon <74883641+sichelh@users.noreply.github.com>
2025-05-21 17:27:19 +0900

    feat : Item Data 추가

---
+++ Assets/Scripts/Item/Data/ItemData.cs
@@ -0,0 +1,46 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum EItemType
+{
+    Consumable,
+    Equipable
+}
+
+public enum ConsumableType
+{
+    Hp,
+    Stamina,
+    RunSpeed,
+    JumpPower,
+}
+
+[Serializable]
+public class ItemDataConsumable
+{
+    public ConsumableType type;
+    public float value;
+}
+
+[CreateAssetMenu(fileName = "ItemData", menuName = "Item Data")]
+public class ItemData : ScriptableObject
+{
+    [Header("Info")]
+    public string itemName;
+    public string itemDescription;
+    public EItemType type;
+    public Sprite icon;
+    public GameObject dropPrefab;
+
+    [Header("Stacking")]
+    public bool canStack;
+    public int maxAmount;
+
+    [Header("Consumable")]
+    public ItemDataConsumable[] consumables;
+
+    [Header("Equipable")]
+    public GameObject equipPrefab;
+}
---
+++ Assets/Scripts/Item/ItemObject.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public interface IInteractable
+{
+    public string GetInteractPrompt();
+    public void OnInteract();
+}
+
+public class ItemObject : MonoBehaviour, IInteractable
+{
+    public ItemData data;
+    Player player;
+
+    private void Awake()
+    {
+        player = FindObjectOfType<Player>();
+    }
+
+    // 아이템 감지되면 정보 출력
+    public string GetInteractPrompt()
+    {
+        string str = $"{data.itemName}\n{data.itemDescription}";
+        return str;
+    }
+
+    // 인터랙션하면 player에게 아이템 데이터를 넘기고 destroy
+    public void OnInteract()
+    {
+        player.itemData = data;
+        player.addItem?.Invoke();
+        Destroy(gameObject);
+    }
+}
